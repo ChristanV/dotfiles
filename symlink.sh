d3ac240e223c8f9b83f02fdb8db26aa9c796d7b6
Christan <ccavrey@gmail.com>
2025-07-10 22:06:20 +0200

    Adding linking script

---
+++ symlink.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+dotfiles=(
+  "nvim" 
+  "starship-config" 
+  "hypr"
+)
+
+for dot in "${dotfiles[@]}"; do
+  echo "Setup symlink for $dot"
+  if [ ! -L ~/.config/"$dot" ]; then
+    ln -sfn ~/dotfiles/"$dot" ~/.config/"$dot"
+    echo "Linked $dot"
+  else
+    echo "$dot already linked"
+  fi
+done
