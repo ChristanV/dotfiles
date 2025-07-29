025af934bd4de8325bf810a24a17e5349a8dc33a
Christan <ccavrey@gmail.com>
2025-07-29 22:49:28 +0200

    Rising changes

--- waybar/scripts/gpu_usage.sh
+++ waybar/scripts/gpu_usage.sh
@@ -2,4 +2,4 @@
 usage=$(nvidia-smi --query-gpu=utilization.gpu,memory.used,memory.total --format=csv,noheader,nounits | head -n1)
 gpu_use=$(echo "$usage" | cut -d',' -f1 | xargs)
 
-echo " GPU ${gpu_use}% "
+echo "󰢮 ${gpu_use}% "
