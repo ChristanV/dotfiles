8727106f2dfecf45ddf28706114823483f2270e3
Christan <ccavrey@gmail.com>
2025-08-04 07:52:21 +0000

    Adding new installs

--- windows/PackageInstalls.ps1
+++ windows/PackageInstalls.ps1
@@ -1,5 +1,23 @@
 # Find packages https://community.chocolatey.org/packages
-$packages = @("git", "vscode", "bruno", "awscli", "azure-cli", "azure-kubelogin", "wsl", "dbeaver", "neovim", "1password", "notion", "wezterm", "manictime", "slack", "devtoys", "powertoys")
+$packages = @(
+  "git",
+  "vscode", 
+  "bruno", 
+  "awscli", 
+  "azure-cli", 
+  "azure-kubelogin", 
+  "wsl", 
+  "dbeaver", 
+  "neovim", 
+  "1password", 
+  "notion", 
+  "wezterm", 
+  "manictime", 
+  "slack", 
+  "devtoys", 
+  "powertoys", 
+  "lightshot"
+)
 
 foreach ($pkg in $packages) {
     Write-Host "Installing $pkg" -ForegroundColor Cyan
