9ba9feaac8a4c1be5d178052b4021d9bd8308e07
christanv <ccavrey@gmail.com>
2025-08-14 18:41:06 +0000

    Updating windows scripts

--- windows/PackageInstalls.ps1
+++ windows/PackageInstalls.ps1
@@ -1,8 +1,24 @@
-# Find packages https://community.chocolatey.org/packages
-$packages = @("git", "vscode", "bruno", "awscli", "azure-cli", "azure-kubelogin", "wsl", "dbeaver", "neovim", "1password", "notion", "wezterm")
+# Find packages https://winget.run/
+$packages = @(
+    "Git.Git",
+    "Microsoft.VisualStudioCode",
+    "Bruno.Bruno",
+    "Amazon.AWSCLI",
+    "Microsoft.AzureCLI",
+    "Microsoft.Azure.Kubelogin",
+    "Microsoft.WSL",
+    "dbeaver.dbeaver",
+    "Neovim.Neovim",
+    "AgileBits.1Password",
+    "Notion.Notion",
+    "wez.wezterm",
+    "Microsoft.PowerToys",
+    "Microsoft.PCManager",
+    "DevToys-app.DevToys"
+)
 
 foreach ($pkg in $packages) {
     Write-Host "Installing $pkg" -ForegroundColor Cyan
-    choco install $pkg -y
-    Write-Host "Installed $pkg" -ForegroundColor Cyan
+    winget install -e --id $pkg --accept-package-agreements --accept-source-agreements
+    Write-Host "Installed $pkg" -ForegroundColor Green
 }
