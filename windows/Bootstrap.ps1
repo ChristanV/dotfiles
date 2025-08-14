c074f26f6f821620e4fac4667f1fc9a4038bcdc7
christanv <ccavrey@gmail.com>
2025-08-14 18:41:06 +0000

    Updating windows scripts

--- windows/Bootstrap.ps1
+++ windows/Bootstrap.ps1
@@ -1,17 +1,2 @@
-Write-Host "Setting execution policy..." -ForegroundColor Cyan
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-Write-Host "Downloading Chocolatey installation script..." -ForegroundColor Cyan
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-$chocoInstallScript = 'https://community.chocolatey.org/install.ps1'
-
-Write-Host "Running installation script..." -ForegroundColor Cyan
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($chocoInstallScript))
-
-Write-Host "Verifying installation..." -ForegroundColor Cyan
-choco -v
-
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
-
-Restart-Computer
