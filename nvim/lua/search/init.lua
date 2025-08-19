f55b464df0bc0b9ff5750500a0bfa4c2018a20b4
christanv <ccavrey@gmail.com>
2025-08-19 17:49:40 +0000

    Allowing telescope to search across .github

--- nvim/lua/search/init.lua
+++ nvim/lua/search/init.lua
@@ -1,26 +1,24 @@
 -- https://github.com/nvim-telescope/telescope.nvim
-require'telescope'.setup({
---  defaults = {
---    mappings = {
---      i = {
---         ['<c-a'] = 'builtin.live_grep'
---      },
---    },
---  },
-  -- pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  -- },
-  -- extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  -- }
+require 'telescope'.setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--follow",        -- Follow symbolic links
+      "--hidden",        -- Search for hidden files
+      "--no-heading",    -- Don't group matches by each file
+      "--with-filename", -- Print the file path with the matched lines
+      "--line-number",   -- Show line numbers
+      "--column",        -- Show column numbers
+      "--smart-case",    -- Smart case search
+
+      "--glob=!**/.git/*",
+      "--glob=!**/.idea/*",
+      "--glob=!**/.vscode/*",
+      "--glob=!**/build/*",
+      "--glob=!**/dist/*",
+      "--glob=!**/yarn.lock",
+      "--glob=!**/package-lock.json",
+      "--glob=**/.github/**", -- Allow searching github workflows
+    },
+  },
 })
