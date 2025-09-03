73bab8bf2bc0bb6f2cbf819ae2e3110462aeedb6
christanv <ccavrey@gmail.com>
2025-09-03 20:48:19 +0000

    Adding copilot neovim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,9 @@ require('search')
 require('uml')
 require('git')
 require('mappings')
+require('ai/copilot')
 
-if os.getenv("NVIM_ENABLE_AI") then
+if os.getenv("NVIM_ENABLE_ALT_AI") then
   require('ai/supermaven')
   require('ai/gen')
   require('ai/chatgpt')
